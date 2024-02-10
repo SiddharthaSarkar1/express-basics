0872d7923454e06a8a6837151d17f32af744c3f5
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-02-10 22:41:40 +0530

    completed crud operations and start with authentication

--- express-mongodb-rest-api/mycontacts-backend/controllers/contactController.js
+++ express-mongodb-rest-api/mycontacts-backend/controllers/contactController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
+  const contact = await Contact.find();
+  res.status(200).json(contact);
 });
 
 //@desc Get individual contact
@@ -13,7 +15,12 @@ const getContacts = asyncHandler(async (req, res) => {
 //@access public
 
 const getContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res.status(200).json(contact);
 });
 
 //@desc Create new contact
@@ -27,7 +34,13 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory");
   }
-  res.status(201).json({ message: "Create contact" });
+
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json({ contact });
 });
 
 //@desc Update contact
@@ -35,7 +48,19 @@ const createContact = asyncHandler(async (req, res) => {
 //@access public
 
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  )
+
+  res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
@@ -43,7 +68,13 @@ const updateContact = asyncHandler(async (req, res) => {
 //@access public
 
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  await Contact.findOneAndDelete(req.params.id);
+  res.status(200).json(contact);
 });
 
 module.exports = {
--- express-mongodb-rest-api/mycontacts-backend/server.js
+++ express-mongodb-rest-api/mycontacts-backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConnecrtion");
 const dotenv = require("dotenv").config();
 
+connectDB();
 const app = express();
 const port = process.env.PORT || 5050;
 
