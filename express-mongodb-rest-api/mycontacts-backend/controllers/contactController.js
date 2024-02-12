90f90bd7a8840b0cd63d09501c6c0314280858c1
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-02-12 22:41:22 +0530

    Implemented JWT user authentication and protecting routes

--- express-mongodb-rest-api/mycontacts-backend/controllers/contactController.js
+++ express-mongodb-rest-api/mycontacts-backend/controllers/contactController.js
@@ -3,16 +3,16 @@ const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 
 const getContacts = asyncHandler(async (req, res) => {
-  const contact = await Contact.find();
+  const contact = await Contact.find({ user_id: req.user.id });
   res.status(200).json(contact);
 });
 
 //@desc Get individual contact
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 
 const getContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
@@ -25,7 +25,7 @@ const getContact = asyncHandler(async (req, res) => {
 
 //@desc Create new contact
 //@route POST /api/contacts
-//@access public
+//@access private
 
 const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is: ", req.body);
@@ -39,13 +39,14 @@ const createContact = asyncHandler(async (req, res) => {
     name,
     email,
     phone,
+    user_id: req.user.id,
   });
   res.status(201).json({ contact });
 });
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
-//@access public
+//@access private
 
 const updateContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
@@ -54,18 +55,23 @@ const updateContact = asyncHandler(async (req, res) => {
     throw new Error("Contact not found");
   }
 
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User dont have permission to update other usercontacts");
+  }
+
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
     req.body,
     { new: true }
-  )
+  );
 
   res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
-//@access public
+//@access private
 
 const deleteContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
@@ -73,6 +79,12 @@ const deleteContact = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact not found");
   }
+
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User dont have permission to update other user contacts");
+  }
+
   await Contact.findOneAndDelete(req.params.id);
   res.status(200).json(contact);
 });
--- express-mongodb-rest-api/mycontacts-backend/controllers/userController.js
+++ express-mongodb-rest-api/mycontacts-backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
-const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 //@desc Register a user
 //@route POST /api/users/register
@@ -44,15 +45,39 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access public
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login user." });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const user = await User.findOne({ email });
+  //compare password with hashed password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Email or Password isnot valid.");
+  }
 });
 
 //@desc Current user info
-//@route POST /api/users/current
+//@route GET /api/users/current
 //@access private
 
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user information." });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
