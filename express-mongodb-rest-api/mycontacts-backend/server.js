02af3849483db41efdf7026b31fb92243551b570
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-10-20 00:09:41 +0530

    added swagger for api documentation

--- express-mongodb-rest-api/mycontacts-backend/server.js
+++ express-mongodb-rest-api/mycontacts-backend/server.js
@@ -3,11 +3,16 @@ const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./config/dbConnecrtion");
 const dotenv = require("dotenv").config();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger-output.json');
+
 connectDB();
 const app = express();
 const port = process.env.PORT || 5050;
 
 app.use(express.json());//This is body parser
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/contacts', require("./routes/contactRoutes"));
 app.use('/api/users', require("./routes/userRoutes"));
 app.use(errorHandler);
